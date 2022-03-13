a74ee4cf93581780118f4adb4e9f033681332e1d
Himanshu Negi <himanshu.21negi@gmail.com>
2022-03-13 23:58:14 +0530

    Palindrome Number addded

---
+++ Topics/palindromeNumber_C.java
@@ -0,0 +1,47 @@
+/*
+Palindrome Number
+https://leetcode.com/problems/palindrome-number/
+Given an integer x, return true if x is palindrome integer.
+
+An integer is a palindrome when it reads the same backward as forward.
+
+For example, 121 is a palindrome while 123 is not.
+
+
+Example 1:
+
+Input: x = 121
+Output: true
+Explanation: 121 reads as 121 from left to right and from right to left.
+Example 2:
+
+Input: x = -121
+Output: false
+Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+Example 3:
+
+Input: x = 10
+Output: false
+Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+* */
+package com.company;
+
+public class palindromeNumber_C {
+    public static void main(String[] args) {
+        int x=-101;
+        System.out.println(isPalindrome(x));
+    }
+    static boolean isPalindrome(int x) {
+        String str=Integer.toString(x);
+        int start=0;
+        int end=str.length()-1;
+        for (int i = 0; i <str.length()/2; i++) {
+            if(str.charAt(start)!=str.charAt(end)){
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+}

84499be08b097854e8a63c944286a9ed225453de
Himanshu Negi <himanshu.21negi@gmail.com>
2022-03-24 23:50:09 +0530

    changes in GCD

--- Topics/Math_2/GCD.java
+++ Topics/Math_2/GCD.java
@@ -1,5 +1,13 @@
 /*
 * HCF/GCD
+* Here we are using the Euclidean formula
+  i.e
+  *
+  * gcd(a,b)=gcd(remainder(b,a),a);
+  * gcd(a,b)=gcd((b%a),a)
+  *
+  * run this code till a becomes zero
+  *
 * HCF=Highest common factor
 * GCD=Greatest common factor*/
 package com.company.Math_2;

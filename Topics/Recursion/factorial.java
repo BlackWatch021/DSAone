a73f07d4eb43953ee28035397098c0d7ef768b03
Himanshu Negi <himanshu.21negi@gmail.com>
2022-03-28 23:11:56 +0530

    Recursion and loop approch added

--- Topics/Recursion/factorial.java
+++ Topics/Recursion/factorial.java
@@ -2,14 +2,26 @@ package com.company.recursion;
 
 public class factorial {
     public static void main(String[] args) {
-        int num=4;
-        System.out.println( factorial(num));
-        
+        int num=6;
+        System.out.println( factorialRec(num));
+        System.out.println(factorailLoop(num));
     }
-    static int factorial(int num){
+    /*Approach 2
+    * Finding the factorial of a number using recursion*/
+    static int factorialRec(int num){
         if(num<=1){
-            return num;
+            return 1;
         }
-        return (num * factorial(num-1));
+        return (num * factorialRec(num-1));
+    }
+    /*Approach 1
+    * Finding the factorial of a number using loop*/
+    static int factorailLoop(int num){
+        int ans=1;
+        while(num>1){
+            ans*=num;
+            --num;
+        }
+        return ans;
     }
 }
